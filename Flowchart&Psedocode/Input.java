6a45575917e9a8f7e9ed6db30515cb44c896f0a9
Prithvi Vijay <133096195+prithvicoder1@users.noreply.github.com>
2026-08-07 14:21:16 +0530

    Add files via upload

---
+++ Flowchart&Psedocode/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter some input");
+        int rollno = input.nextInt();
+        System.out.println("Your roll number is " + rollno);
+
+        int a = 10;
+    }
+}
---
+++ Flowchart&Psedocode/primitives.java
@@ -0,0 +1,14 @@
+public class primitives {
+    public static void main(String[] args){
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f; // uisng f is float
+        double largeDecimalNumbers = 6433436.63224;
+        long largeInteger = 6376462687286458L; //using L is long  
+        boolean check = true;
+
+        //String is not primitive 
+        String name = "Kunal";
+    }
+}
+// Primitive : are the type of data type which we can not break till feature 
