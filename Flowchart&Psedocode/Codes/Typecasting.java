a4b65308b99a216a64ba11203b17d8091d8c2c52
Prithvi Vijay <133096195+prithvicoder1@users.noreply.github.com>
2026-08-07 14:38:25 +0530

    Delete Flowchart&Psedocode directory

--- Flowchart&Psedocode/Codes/Typecasting.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Typecasting {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        float num = input.nextFloat();
-        System.out.println(num);
-    }
-}
-// automatically Type conversion happens in the typecasting
